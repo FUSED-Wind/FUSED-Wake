48ddb45a167b5cbac09ce2a534c10726690f4bcc
Pierre-Elouan <pe@retho.re>
2016-01-20 16:22:21 +0100

    preparing for docs

--- fusedwake/sdwm/DWM_init_dict.py
+++ fusedwake/sdwm/DWM_init_dict.py
@@ -9,24 +9,49 @@ def init(WF):
 
         Inputs
         ----------
-        WF (instance of class): Instance of class WindFarm holding wind farm layout and coordinate
+        WF: WindFarm
+            Instance of class WindFarm holding wind farm layout and coordinate
 
         Outputs
         ----------
-       deficits: dict(nWT) holding a list of deficits contributions from upstream wakes
-       turb:  dict(nWT) holding a list of turbulence intensities contributions from upstream wakes
-       inlets_ffor: dict(nWT) holding a list of array containing the flow field in the fixed frame of reference from upstream wakes contributions
-       inlets_ffor_deficits: dict(nWT) holding a list of array containing the flow field in the fixed frame of reference from upstream wakes contributions at the rotor position
-       inlets_ffor_turb: dict(nWT) holding a list of array containing the turbulence field in the fixed frame of reference from upstream wakes contributions at the rotor position
-       out: dict(nWT), holding the main sDWM model outputs i.e mean power from BEM, mean power estimated from power curve,
-        mean rotor averaged wind speed, mean rotor average turbulence intensity, mean thrust coefficient from BEM and from power curve
-       DWM: dict(nWT)   list containing full outputs of the sDWM (including flow field in ffor and mfor) See description of DWM_outputs for more details
-       ID_waked: dict(nWT) holding list of upstream turbine index for each turbine in the wind farm
-       ID_wake_adj: dict(nWT) holding list of downstream turbine index for each turbine in the wind farm
-       Farm_p_out (float) : wind farm power produced
-       WT_p_out (np.array float): power produced by each turbine in the wind farm
-       Vel_out (np.array float): mean rotor averaged aggregated velocity for each turbine in the wind farm
-       Pos_out (np.array float): outputs coordinates X,Y of wind turbines
+        deficits: dict(nWT)
+            holding a list of deficits contributions from upstream wakes
+        turb:  dict(nWT)
+            holding a list of turbulence intensities contributions from upstream
+            wakes
+        inlets_ffor: dict(nWT)
+            holding a list of array containing the flow field in the fixed frame
+            of reference from upstream wakes contributions
+        inlets_ffor_deficits: dict(nWT)
+            holding a list of array containing the flow field in the fixed frame
+            of reference from upstream wakes contributions at the rotor position
+        inlets_ffor_turb: dict(nWT)
+            holding a list of array containing the turbulence field in the fixed
+            frame of reference from upstream wakes contributions at the rotor
+            position
+        out: dict(nWT)
+            holding the main sDWM model outputs (i.e mean power from BEM, mean
+            power estimated from power curve, mean rotor averaged wind speed,
+            mean rotor average turbulence intensity, mean thrust coefficient
+            from BEM and from power curve)
+        DWM: dict(nWT)
+            list containing full outputs of the sDWM (including flow field in
+            ffor and mfor) See description of DWM_outputs for more details
+        ID_waked: dict(nWT)
+            holding list of upstream turbine index for each turbine in the wind
+            farm
+        ID_wake_adj: dict(nWT)
+            holding list of downstream turbine index for each turbine in the wind
+            farm
+        Farm_p_out: float
+            wind farm power produced
+        WT_p_out: np.array(float)
+            power produced by each turbine in the wind farm
+        Vel_out: np.array(float)
+            mean rotor averaged aggregated velocity for each turbine in the wind
+            farm
+        Pos_out: np.array(float)
+            outputs coordinates X,Y of wind turbines
     """
     ## Init dictionnaries involved in full outputs
     deficits = dict()
@@ -45,7 +70,7 @@ def init(WF):
     for iC in range(WF.nWT):
         inlets_ffor_turb[str(iC)]=[]    ## inlets dictionnary for turbulence build up
     out = dict()    ## outputs dictionnary to the wake model
-    DWM=dict()## flow field dictionnary outputs to wake model
+    DWM = dict()## flow field dictionnary outputs to wake model
     ID_waked=dict()
     for iC in range(WF.nWT):
         ID_waked[str(iC)]=[]    # ID_Wake ordered by wind direction
